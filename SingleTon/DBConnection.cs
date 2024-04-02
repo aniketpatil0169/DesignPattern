d812b9de432af4ec0f107319d085c890e8814615
aniketpatil0169 <148536939+aniketpatil0169@users.noreply.github.com>
2024-04-02 19:05:56 +0530

    Add files via upload

---
+++ SingleTon/DBConnection.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleTon
+{
+    //public class DBConnection
+    //{
+    //    private static DBConnection _obj = null;
+
+    //    private static object _o = new object();
+    //    private DBConnection() { }
+
+    //    public static DBConnection Instance
+    //    {
+    //        get
+    //        {
+    //            if (_obj == null)
+    //             {
+    //            lock (_o)
+    //            {
+    //                if (_obj == null)
+    //                {
+    //                    _obj = new DBConnection();
+    //                }
+    //                return _obj;
+    //            }
+    //        }
+    //        return _obj;
+    //    }
+    //}
+    // }
+
+    public class DBConnection
+    {
+        private static DBConnection _obj = null;
+
+        private static object _o = new object();
+
+        private static int _counter = 1;
+        private DBConnection() { }
+
+        public static DBConnection Instance
+        {
+            get
+            {
+                if (_counter <= 5)
+                {
+                    _obj = new DBConnection();
+                    _counter++;
+                }
+                else 
+                {
+                    Console.WriteLine("Only 5 objects are possible");
+                }
+                return _obj;
+            }
+        }
+    }
+}
---
+++ SingleTon/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleTon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DBConnection con1 =  DBConnection.Instance;
+
+            DBConnection con2 =  DBConnection.Instance;
+
+            DBConnection con3 =  DBConnection.Instance;
+
+            if (con1.Equals(con2) && con2.Equals(con3))
+            {
+                Console.WriteLine("con1, con2 & con3 objects are EQUAL");
+            }
+            else
+            {
+                Console.WriteLine("con1, con2 & con3 objects are NOT EQUAL");
+            }
+
+            DBConnection con4 = DBConnection.Instance;
+
+            DBConnection con5 = DBConnection.Instance;
+
+            DBConnection con6 = DBConnection.Instance; // this is not possible
+
+
+            Console.ReadLine();
+        }
+    }
+}
